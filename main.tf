3f5ab47425673f76933942a2d341bf40fade3617
singhish <singhish512@outlook.com>
2024-11-24 17:00:13 -0600

    give vms random pet names instead of indices, none of them are special

--- main.tf
+++ main.tf
@@ -3,10 +3,16 @@ resource "hcloud_ssh_key" "main" {
   public_key = var.vm_ssh_key
 }
 
+resource "random_pet" "hcloud_vm_names" {
+  count = var.hcloud_num_vms
+
+  prefix = "${var.org_name}-${var.hcloud_location}-vm"
+}
+
 resource "hcloud_server" "server" {
   count = var.hcloud_num_vms
 
-  name        = "${var.org_name}-${var.hcloud_location}-vm-${count.index}"
+  name        = random_pet.hcloud_vm_names[count.index].id
   server_type = "cpx11"
   location    = var.hcloud_location
   image       = "ubuntu-24.04"
